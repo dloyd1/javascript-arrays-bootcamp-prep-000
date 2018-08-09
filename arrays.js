559cfbd1dd404324c960abee480514a792104bbe
Daniel Loyd <dhloyd@icloud.com>
2018-08-09 04:34:06 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray([1]) {}
+function addElementToBeginningOfArray() {}
 
 function destructivelyAddElementToBeginningOfArray() {}
 
